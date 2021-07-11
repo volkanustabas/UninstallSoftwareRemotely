f24c346e24732b2b2484a74126c1450d2603872d
Volkan <Volkan@VLKN08PC01>
2021-07-11 18:05:29 +0300

    Add project files.

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace UninstallSoftwareRemotely
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.tb_remoteIpAdress = new System.Windows.Forms.TextBox();
+            this.lbl_info_remoteIpAdress = new System.Windows.Forms.Label();
+            this.lv_programList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cms_delete = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.bw_search = new System.ComponentModel.BackgroundWorker();
+            this.bw_delete = new System.ComponentModel.BackgroundWorker();
+            this.cms_delete.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb_remoteIpAdress
+            // 
+            this.tb_remoteIpAdress.Location = new System.Drawing.Point(16, 29);
+            this.tb_remoteIpAdress.Name = "tb_remoteIpAdress";
+            this.tb_remoteIpAdress.Size = new System.Drawing.Size(156, 20);
+            this.tb_remoteIpAdress.TabIndex = 0;
+            // 
+            // lbl_info_remoteIpAdress
+            // 
+            this.lbl_info_remoteIpAdress.AutoSize = true;
+            this.lbl_info_remoteIpAdress.Location = new System.Drawing.Point(13, 13);
+            this.lbl_info_remoteIpAdress.Name = "lbl_info_remoteIpAdress";
+            this.lbl_info_remoteIpAdress.Size = new System.Drawing.Size(91, 13);
+            this.lbl_info_remoteIpAdress.TabIndex = 1;
+            this.lbl_info_remoteIpAdress.Text = "Remote Ip Adress";
+            // 
+            // lv_programList
+            // 
+            this.lv_programList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lv_programList.ContextMenuStrip = this.cms_delete;
+            this.lv_programList.FullRowSelect = true;
+            this.lv_programList.HideSelection = false;
+            this.lv_programList.Location = new System.Drawing.Point(188, 29);
+            this.lv_programList.Name = "lv_programList";
+            this.lv_programList.Size = new System.Drawing.Size(716, 498);
+            this.lv_programList.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.lv_programList.TabIndex = 2;
+            this.lv_programList.UseCompatibleStateImageBehavior = false;
+            this.lv_programList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Version";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Vendor";
+            this.columnHeader3.Width = 150;
+            // 
+            // cms_delete
+            // 
+            this.cms_delete.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem});
+            this.cms_delete.Name = "cms_delete";
+            this.cms_delete.Size = new System.Drawing.Size(108, 26);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(16, 56);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(156, 23);
+            this.btn_search.TabIndex = 3;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // bw_search
+            // 
+            this.bw_search.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_search_DoWork);
+            this.bw_search.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bw_search_RunWorkerCompleted);
+            // 
+            // bw_delete
+            // 
+            this.bw_delete.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_delete_DoWork);
+            this.bw_delete.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bw_delete_RunWorkerCompleted);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 539);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.lv_programList);
+            this.Controls.Add(this.lbl_info_remoteIpAdress);
+            this.Controls.Add(this.tb_remoteIpAdress);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "Uninstall Software Remotely";
+            this.cms_delete.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_remoteIpAdress;
+        private System.Windows.Forms.Label lbl_info_remoteIpAdress;
+        private System.Windows.Forms.ListView lv_programList;
+        private System.Windows.Forms.Button btn_search;
+        private System.ComponentModel.BackgroundWorker bw_search;
+        private System.Windows.Forms.ContextMenuStrip cms_delete;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker bw_delete;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace UninstallSoftwareRemotely
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private static bool PingHost(string remoteIpAdress)
+        {
+            var pingable = false;
+            var pinger = new Ping();
+
+            try
+            {
+                var reply = pinger.Send(remoteIpAdress);
+                if (reply != null) pingable = reply.Status == IPStatus.Success;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return pingable;
+        }
+
+        private static void AutoSizeColumnList(ListView listView)
+        {
+            listView.BeginUpdate();
+
+            var columnSize = new Dictionary<int, int>();
+
+            listView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            foreach (ColumnHeader colHeader in listView.Columns)
+                columnSize.Add(colHeader.Index, colHeader.Width);
+
+            listView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+            foreach (ColumnHeader colHeader in listView.Columns)
+                colHeader.Width = Math.Max(columnSize.TryGetValue(colHeader.Index, out var nColWidth) ? nColWidth : 50,
+                    colHeader.Width);
+
+            listView.EndUpdate();
+        }
+
+        private static bool DeleteProgram(string programName, string computerName)
+        {
+            try
+            {
+                var connection = new ConnectionOptions();
+
+                var scope = new ManagementScope("\\\\" + computerName + "\\root\\cimv2", connection);
+
+                scope.Connect();
+
+                var query = new ObjectQuery("SELECT * FROM Win32_Product WHERE Name ='" + programName + "'");
+
+                var searcher = new ManagementObjectSearcher(scope, query);
+
+                foreach (var o in searcher.Get())
+                {
+                    var queryObj = (ManagementObject) o;
+
+                    try
+                    {
+                        if (queryObj["Name"].ToString() == programName)
+                        {
+                            var hr = queryObj.InvokeMethod("Uninstall", null);
+                            return (bool) hr;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        //
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static void ListPrograms(string computerName, ListView lv)
+        {
+            try
+            {
+                var connection = new ConnectionOptions();
+                var scope = new ManagementScope("\\\\" + computerName + "\\root\\cimv2", connection);
+                scope.Connect();
+                var query = new ObjectQuery("SELECT * FROM Win32_Product WHERE (NOT Vendor LIKE '%Microsoft%')");
+                var searcher = new ManagementObjectSearcher(scope, query);
+
+                foreach (var o in searcher.Get())
+                {
+                    var queryObj = (ManagementObject) o;
+
+                    var item1 = new ListViewItem {Text = queryObj["Name"].ToString()};
+                    item1.SubItems.Add(queryObj["Version"].ToString());
+                    item1.SubItems.Add(queryObj["Vendor"].ToString());
+                    lv.Items.Add(item1);
+                }
+            }
+            catch (ManagementException me)
+            {
+                MessageBox.Show(@"Error: " + me, @"Warning!");
+                throw;
+            }
+        }
+
+        #region Search
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            var ipAdress = tb_remoteIpAdress.Text.Trim();
+
+            if (ipAdress == "")
+            {
+                MessageBox.Show(@"Ip Adress Not Null",@"Info");
+            }
+
+            else
+            {
+                if (PingHost(ipAdress))
+                {
+                    lv_programList.Items.Clear();
+
+                    bw_search.RunWorkerAsync();
+                    btn_search.Enabled = false;
+                    tb_remoteIpAdress.Enabled = false;
+                    lv_programList.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"Ping Error!" , @"Info");
+                }
+            }
+        }
+
+        private void bw_search_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BeginInvoke((MethodInvoker) delegate { });
+
+            ListPrograms(tb_remoteIpAdress.Text.Trim(), lv_programList);
+        }
+
+        private void bw_search_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            AutoSizeColumnList(lv_programList);
+            btn_search.Enabled = true;
+            tb_remoteIpAdress.Enabled = true;
+            lv_programList.Enabled = true;
+        }
+
+        #endregion
+
+        #region Delete
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ipAdress = tb_remoteIpAdress.Text.Trim();
+
+            if (ipAdress == "")
+            {
+                MessageBox.Show(@"Ip Adress Not Null",@"Info");
+            }
+
+            else
+            {
+                if (PingHost(ipAdress))
+                {
+                    bw_delete.RunWorkerAsync();
+                    btn_search.Enabled = false;
+                    tb_remoteIpAdress.Enabled = false;
+                    lv_programList.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"Ping Error!" , @"Info");
+                }
+            }
+        }
+
+        private void bw_delete_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BeginInvoke((MethodInvoker) delegate { });
+
+            var programNameForDelete = lv_programList.SelectedItems[0].SubItems[0].Text;
+            var ipAdressForDelete = tb_remoteIpAdress.Text.Trim();
+
+            DeleteProgram(programNameForDelete, ipAdressForDelete);
+        }
+
+        private void bw_delete_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show(@"Program Deleted", @"Info");
+            btn_search.Enabled = true;
+            tb_remoteIpAdress.Enabled = true;
+            lv_programList.Enabled = true;
+        }
+
+        #endregion
+    }
+}
